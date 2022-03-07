175c047e3d3c043147b922d80da5de9e05615e67
Erdős György <95422855+TheMortail@users.noreply.github.com>
2022-03-07 06:25:16 -0800

    Add files via upload

---
+++ javaTesting/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_record (user_id, name, age, address) VALUES (1, 'Run Géza', 25, 'Budapest');
+INSERT INTO user_record (user_id, name, age, address) VALUES (2, 'Run Sándor', 30, 'Jászegérleső');
+INSERT INTO user_record (user_id, name, age, address) VALUES (3, 'Run Kelemen', 49, 'Dunabérbatyk');
