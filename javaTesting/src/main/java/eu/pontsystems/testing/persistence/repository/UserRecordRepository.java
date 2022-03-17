14abef18daa12b046cbd664cca4aa61e6db64f4e
Erdős György <95422855+TheMortail@users.noreply.github.com>
2022-03-17 05:31:59 -0700

    java genocide

--- javaTesting/src/main/java/eu/pontsystems/testing/persistence/repository/UserRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package eu.pontsystems.testing.persistence.repository;
-
-import eu.pontsystems.testing.persistence.entity.UserRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRecordRepository extends JpaRepository<UserRecord, Long> {
-}
