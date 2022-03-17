872ff464505deefae98feeee1fdcae7f4a8f1999
Erdős György <95422855+TheMortail@users.noreply.github.com>
2022-03-17 05:28:55 -0700

    Delete javaTesting directory

--- javaTesting/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE if not exists user_record(
-    user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    age int,
-    address VARCHAR(50)
-);
--- javaTesting/src/main/resources/testDB/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO user_record (user_id, name, age, address) VALUES (1, 'Teszt Géza', 20, 'Zalabér-batyk');
-INSERT INTO user_record (user_id, name, age, address) VALUES (2, 'Teszt Sándor', 35, 'Pusztakotkodács');
-INSERT INTO user_record (user_id, name, age, address) VALUES (3, 'Teszt Aladár', 10, 'Köd');
